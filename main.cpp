192a40063e13cade86c5ff2db05db042f7cba165
Pacharoth <pacharothroeun01@gmail.com>
2020-05-08 12:02:42 +0700

    another version

--- main.cpp
+++ main.cpp
@@ -11,16 +11,26 @@ struct List{
     int n;
     Element *head;
     Element *tail;
+    vo
 };
-//create List
- List* createList(){
-    List *ls;
-    ls=new List();
-    ls->n=0;
-    ls->head=NULL;
-    ls->tail=NULL;
-    return ls;
-}
+class algo_LinkList{
+    public:
+        //create List
+        List* createList(){
+            List *ls;
+            ls=new List();
+            ls->n=0;
+            ls->head=NULL;
+            ls->tail=NULL;
+            return ls;
+        }
+        //insert begin
+        void insertBegin(List *ls,string newData);
+        //insert end
+        void insertEnd(List *ls,string newData);
+        //show data
+        void displayList(List *ls);
+};
 class mainMenu{
     public:
        void designMenu();//display the main Menu
@@ -32,22 +42,26 @@ class Automata{
 
 
 };
+//All load edit delete save to database
 class Database{
     public:
+        //create database
         void createDatabase();
-        void insertData(string data);
+        //insert to database
+        void insertData(List *ls,string data);
+        //Load database
+        void loadData();
     private:
         string sql;
         sqlite3 *db;
         char exit=0;
         char hey=0;
         char *message;
-
 };
 int main(){
     //declare variable 
     int choice;
-    string data;
+    List *ls;
     mainMenu mainMenu;
     Database database;
 
@@ -66,9 +80,11 @@ int main(){
             mainMenu.chooseLanguage();
             cout<<"Insert language:";
             cin>>data;
+            
         }
         else if (choice==6)
         {
+            database.insertData(ls,data);
             cout<<"Exit the program\n\n";
             break;
         }
@@ -88,13 +104,10 @@ void mainMenu::chooseLanguage(){
     cout<<"1.Choose language below";
     cout<<"L={W|W={ab}^n} ,n>0\n";
 }
-void Database::insertData(string data){
-    
-}
 void Database::createDatabase(){
     sql="create table Language("
         "lid integer primary key autoincrement,"
-        "data char(100) not null);";
+        "data char(100) not null unique);";
     //create database
     exit=sqlite3_open("automata.db",&db);
     //execute the query
@@ -109,6 +122,23 @@ void Database::createDatabase(){
         cout<<"Fail create Table\n";
     }else{
         cout<<"Successful create the table\n";
+    }
+    sqlite3_close(db);
+}
+void Database::insertData(List *ls,string data){
+    Element *tmp;
+    tmp =ls->head
+    while(tmp!=NULL){
+        sql="insert into Language(data) values('"+tmp->data+"');";
+        tmp->next;
+    }
+    // sql="insert into Language(data) values('"+tmp->data+"');";
+    exit=sqlite3_open("automata.db",&db);
+    exit=sqlite3_exec(db,sql.c_str(),0,NULL,&message);
+    if(exit!=SQLITE_OK){
+        cout<<"Fail record\n";
+    }else{
+        cout<<"Successful record\n";
     }
     sqlite3_close(db);
 }
